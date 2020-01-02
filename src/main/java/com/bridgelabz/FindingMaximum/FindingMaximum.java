3437220c966791d4f58fcb276cf2f2ffe19f3c70
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-01-02 19:27:23 +0530

    Refactored: Created generic class to take 3 variables of Generic Type

--- src/main/java/com/bridgelabz/FindingMaximum/FindingMaximum.java
+++ src/main/java/com/bridgelabz/FindingMaximum/FindingMaximum.java
@@ -1,15 +1,34 @@
 package com.bridgelabz.FindingMaximum;
 
-public class FindingMaximum <E extends Comparable> {
+public class FindingMaximum <E extends Comparable>{
+    E value1;
+    E value2;
+    E value3;
 
-    public E printMaximum(E value1, E value2, E value3) {
-        E maximum = value1;
+    public FindingMaximum(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public FindingMaximum() {
+
+
+    }
+
+    public <E extends Comparable> E printMaximum(E value1, E value2, E value3) {
+         E maximum = (E) value1;
         if (value2.compareTo(maximum) > 0) {
-            maximum = value2;
+            maximum = (E) value2;
         }
         if (value3.compareTo(maximum) > 0) {
-            maximum = value3;
+            maximum = (E) value3;
         }
+        return maximum;
+    }
+
+    public Object printMaximumValue() {
+        E maximum = printMaximum(value1, value2, value3);
         return maximum;
     }
 }
--- src/test/java/com/bridgelabz/Test/FindingMaximumTest.java
+++ src/test/java/com/bridgelabz/Test/FindingMaximumTest.java
@@ -4,20 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FindingMaximumTest {
+
+
     @Test
     public void WhenGivenIntValues_IfFirstPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         Integer result = (Integer) maximum.printMaximum(50, 20, 10);
-        Integer expectedResult = 50;
-        Assert.assertEquals(expectedResult, Integer.valueOf(result));
+        Assert.assertEquals(Integer.valueOf(50), result);
     }
 
     @Test
     public void WhenGivenIntValues_IfSecondPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         Integer result = (Integer) maximum.printMaximum(10, 50, 20);
-        Integer expectedResult = 50;
-        Assert.assertEquals(expectedResult, Integer.valueOf(result));
+        Assert.assertEquals(Integer.valueOf(50), result);
 
     }
 
@@ -25,54 +25,69 @@ public class FindingMaximumTest {
     public void WhenGivenIntValues_IfThirdPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         Integer result = (Integer) maximum.printMaximum(10, 20, 50);
-        Integer expectedResult = 50;
-        Assert.assertEquals(expectedResult, Integer.valueOf(result));
+        Assert.assertEquals(Integer.valueOf(50), result);
     }
 
     @Test
     public void WhenGivenFloatValues_IfFirstPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         Float result = (Float) maximum.printMaximum(50.5f, 20.5f, 10.5f);
-        Float expectedResult = 50.5f;
-        Assert.assertEquals(expectedResult, Float.valueOf(result));
+        Assert.assertEquals(Float.valueOf(50.5f), result);
     }
 
     @Test
     public void WhenGivenFloatValues_IfSecondPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         Float result = (Float) maximum.printMaximum(20.5f, 50.5f, 10.5f);
-        Float expectedResult = 50.5f;
-        Assert.assertEquals(expectedResult, Float.valueOf(result));
+        Assert.assertEquals(Float.valueOf(50.5f), result);
     }
 
     @Test
     public void WhenGivenFloatValues_IfThirdPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         Float result = (Float) maximum.printMaximum(20.5f, 10.5f, 50.5f);
-        Float expectedResult = 50.5f;
-        Assert.assertEquals(expectedResult, Float.valueOf(result));
+        Assert.assertEquals(Float.valueOf(50.5f), result);
     }
 
     @Test
     public void WhenGivenStringValues_IfFirstPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         String result = (String) maximum.printMaximum("Java", "C", ".Net");
-        String expectedResult = "Java";
-        Assert.assertEquals(expectedResult, String.valueOf(result));
+        Assert.assertEquals(String.valueOf("Java"), result);
     }
 
     @Test
     public void WhenGivenStringValues_IfSecondPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         String result = (String) maximum.printMaximum("C", "Java", ".Net");
-        String expectedResult = "Java";
-        Assert.assertEquals(expectedResult, String.valueOf(result));
+        Assert.assertEquals(String.valueOf("Java"), result);
     }
+
     @Test
     public void WhenGivenStringValues_IfThirdPosHasMax_ShouldReturnMaximum() {
         FindingMaximum maximum = new FindingMaximum();
         String result = (String) maximum.printMaximum("C", ".Net", "Java");
-        String expectedResult = "Java";
-        Assert.assertEquals(expectedResult, String.valueOf(result));
+        Assert.assertEquals(String.valueOf("Java"), result);
+    }
+
+    @Test
+    public void WhenGivenGeneric_ShouldReturnMaxInt() {
+        FindingMaximum maximum = new FindingMaximum(50, 20, 10);
+        Object result = maximum.printMaximumValue();
+        Assert.assertEquals(Integer.valueOf(50), result);
+
+    }
+
+    @Test
+    public void WhenGivenGeneric_ShouldReturnMaxFloat() {
+        FindingMaximum maximum = new FindingMaximum(50.5f, 20.5f, 10.5f);
+        Object result = maximum.printMaximumValue();
+        Assert.assertEquals(Float.valueOf(50.5f), result);
+    }
+    @Test
+    public void WhenGivenGeneric_ShouldReturnMaxString() {
+        FindingMaximum maximum = new FindingMaximum("Java","C",".Net");
+        Object result = maximum.printMaximumValue();
+        Assert.assertEquals(String.valueOf("Java"), result);
     }
 }
