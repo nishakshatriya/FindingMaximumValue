0a7af368266f1f53ab74c1adfd94b8b641555fb8
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-01-02 16:54:39 +0530

    Added: Fuctionality to find Maximum float value and TestCase to Validate it

--- src/main/java/com/bridgelabz/FindingMaximum/FindingMaximum.java
+++ src/main/java/com/bridgelabz/FindingMaximum/FindingMaximum.java
@@ -2,7 +2,7 @@ package com.bridgelabz.FindingMaximum;
 
 public class FindingMaximum {
 
-    public Integer printMaximum(Integer value1, Integer value2, Integer value3) {
+    public Integer printMaximumInteger(Integer value1, Integer value2, Integer value3) {
         Integer maximum = value1;
         if(value2.compareTo(maximum)>0) {
             maximum = value2;
@@ -10,7 +10,17 @@ public class FindingMaximum {
         if (value3.compareTo(maximum)>0) {
             maximum = value3;
         }
+        return maximum;
+    }
 
+    public Float printMaximumFloat(Float value1, Float value2, Float value3) {
+        Float maximum = value1;
+        if(value2.compareTo(maximum)>0) {
+            maximum = value2;
+        }
+        if (value3.compareTo(maximum)>0) {
+            maximum = value3;
+        }
         return maximum;
     }
 }
--- src/test/java/com/bridgelabz/Test/FindingMaximumTest.java
+++ src/test/java/com/bridgelabz/Test/FindingMaximumTest.java
@@ -5,27 +5,50 @@ import org.junit.Test;
 
 public class FindingMaximumTest {
     @Test
-    public void WhenGivenThreeValues_IfFirstLocationHasMax_ShouldReturnMaximum() {
-        FindingMaximum maximum =new FindingMaximum();
-        Integer result = (Integer) maximum.printMaximum(50, 20 , 10);
-        Integer expectedResult=50;
-        Assert.assertEquals(expectedResult,Integer.valueOf(result));
+    public void WhenGivenIntValues_IfFirstPosHasMax_ShouldReturnMaximum() {
+        FindingMaximum maximum = new FindingMaximum();
+        Integer result = (Integer) maximum.printMaximumInteger(50, 20, 10);
+        Integer expectedResult = 50;
+        Assert.assertEquals(expectedResult, Integer.valueOf(result));
     }
 
     @Test
-    public void WhenGivenThreeValues_IfSecondLocationHasMax_ShouldReturnMaximum() {
-        FindingMaximum maximum =new FindingMaximum();
-        Integer result = (Integer) maximum.printMaximum(10, 50 , 20);
-        Integer expectedResult=50;
-        Assert.assertEquals(expectedResult,Integer.valueOf(result));
+    public void WhenGivenIntValues_IfSecondPosHasMax_ShouldReturnMaximum() {
+        FindingMaximum maximum = new FindingMaximum();
+        Integer result = (Integer) maximum.printMaximumInteger(10, 50, 20);
+        Integer expectedResult = 50;
+        Assert.assertEquals(expectedResult, Integer.valueOf(result));
 
     }
 
     @Test
-    public void WhenGivenThreeValues_IfThirdPosHasMax_ShouldReturnMaximum() {
-        FindingMaximum maximum =new FindingMaximum();
-        Integer result = (Integer) maximum.printMaximum(10, 20 , 50);
-        Integer expectedResult=50;
-        Assert.assertEquals(expectedResult,Integer.valueOf(result));
+    public void WhenGivenIntValues_IfThirdPosHasMax_ShouldReturnMaximum() {
+        FindingMaximum maximum = new FindingMaximum();
+        Integer result = (Integer) maximum.printMaximumInteger(10, 20, 50);
+        Integer expectedResult = 50;
+        Assert.assertEquals(expectedResult, Integer.valueOf(result));
+    }
+
+    @Test
+    public void WhenGivenFloatValues_IfFirstPosHasMax_ShouldReturnMaximum() {
+        FindingMaximum maximum = new FindingMaximum();
+        Float result = (Float) maximum.printMaximumFloat(50.5f, 20.5f, 10.5f);
+        Float expectedResult = 50.5f;
+        Assert.assertEquals(expectedResult, Float.valueOf(result));
+    }
+
+    @Test
+    public void WhenGivenFloatValues_IfSecondPosHasMax_ShouldReturnMaximum() {
+        FindingMaximum maximum = new FindingMaximum();
+        Float result = (Float) maximum.printMaximumFloat(20.5f, 50.5f, 10.5f);
+        Float expectedResult = 50.5f;
+        Assert.assertEquals(expectedResult, Float.valueOf(result));
+    }
+    @Test
+    public void WhenGivenFloatValues_IfThirdPosHasMax_ShouldReturnMaximum() {
+        FindingMaximum maximum = new FindingMaximum();
+        Float result = (Float) maximum.printMaximumFloat(20.5f, 10.5f, 50.5f);
+        Float expectedResult = 50.5f;
+        Assert.assertEquals(expectedResult, Float.valueOf(result));
     }
 }
